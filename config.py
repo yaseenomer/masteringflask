57de473add8fa61e07ed4c00953e241160925743
yaseenomer <yaseeno373@gmail.com>
2018-08-09 19:10:52 +0300

    Add files via upload
    commit 2

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config(object):
+ pass
+
+
+class ProdConfig(Config):
+ pass
+
+
+class DevConfig(Config):
+ DEBUG = True
+ SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+ SQLALCHEMY_TRACK_MODIFICATIONS = False
